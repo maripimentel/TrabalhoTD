9f274abf56d781b10896d540e1e72c568127b850
Mariana Pimentel <maripimentelms@gmail.com>
2017-06-25 11:45:39 -0300

    Não sensível ao caso (maiúsculo e minúsculo) e com múltiplos usuários
    Falta:
    - Checar se a comparação pode ser feita sem os espaços.

--- proxy3.py
+++ proxy3.py
@@ -16,11 +16,7 @@ def proxy (conexao, cliente, pedido):
 		porta = 80
 		url_raiz = url_completa[:posicao]
 		urlCacheadaFlag = False
-		print "***********************\n"
 		for j in range (0, len(urls)):
-			print "url completa: ", url_completa, "\n"
-			print "urls: ",urls[j], "\n"
-			print "comparacao: ", url_completa in urls[j], "\n"
 			if url_completa == urls[j]:
 				urlCacheadaFlag = True
 				break
@@ -28,24 +24,17 @@ def proxy (conexao, cliente, pedido):
 		pass
 
 	for i in range(0, len(blacklist)):
-		#print "blacklist: ", blacklist[i]
-		#print "url: ", url
-		#print "comparacao: ", (blacklist[i] in url)
 		if blacklist[i] in url:
-			print "Site na blacklist: ", url
 			conexao.send(str.encode("HTTP/1.1 400 Bad Request\nContent-Type:text/html\n\n<html><body> Acesso Negado! </body></html>\n"))
 			if conexao:
 				conexao.close()
 			arq=open('log.txt', 'a')
 			arq.write(time.strftime('%d/%m/%Y %H:%M:%S')+" - Encaminhamento Recusado: "+url+" presente na blacklist\n")
 			arq.close()
-			sys.exit("SAINDO DA THREAD")	
+			sys.exit(1)	
 
 	WhiteListFlag = False
 	for i in range(0, len(whitelist)):
-		#print "whitelist: ", whitelist[i]
-		#print "url: ", url
-		#print "comparacao: ", (whitelist[i] in url)
 		if whitelist[i] in url:
 			WhiteListFlag = True
 			arq=open('log.txt', 'a')
@@ -53,37 +42,24 @@ def proxy (conexao, cliente, pedido):
 			arq.close()
 			break
 
-	#print "O PROBLEMA EH O GETHOST: ", url_raiz
 	try:
 		internet = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		internet.connect((url_raiz, porta))
 		internet.send(pedido)
-		#print "*** Pedido ***"
-		#print pedido
-		#print "*** Fim Pedido ***\n\n"
+
 		resposta_final = ""
 		while True:
 			if urlCacheadaFlag:
-				print "USOU CACHE\n\n\n\n"
 				resposta_final = respostas[j]
 				break
-			print "NAO USOU CACHE\n\n\n\n"
 			resposta = internet.recv(maximo_dados)
-			#print "*** Resposta ***"
-			#print resposta
-			#print "*** Fim Resposta ***\n\n"
 			if resposta:
 				resposta_final += resposta
-				#print "*** Resposta Final ***"
-				#print resposta_final
-				#print "*** Fim Resposta Final ***\n\n"
+				resposta_analise = resposta.upper()
 				if not WhiteListFlag:
 					for i in range(0, len(termo)):
-						#print "******** TERMO *********"
-						#print termo[i]
-						posicao = resposta.find(" "+termo[i]+" ")
+						posicao = resposta_analise.find(" "+termo[i]+" ")
 						if posicao!=-1:
-							print "Termo na blacklist: ", termo[i]
 							conexao.send(str.encode("HTTP/1.1 400 Bad Request\nContent-Type:text/html\n\n<html><body> Conteudo Restrito! </body></html>\n"))
 							if internet:
 								internet.close()
@@ -99,7 +75,6 @@ def proxy (conexao, cliente, pedido):
 		if not urlCacheadaFlag:
 			urls.append(url_completa)
 			respostas.append(resposta_final)
-			print "PASSOU AQUIIIIII \n\n\n\n\n" , len(urls), "\t", len(respostas), "\n"
 		conexao.send(resposta_final)
 		internet.close()
 		conexao.close()
@@ -115,9 +90,8 @@ def proxy (conexao, cliente, pedido):
 		print "Erro: ", mensagem
 		sys.exit(1)
 
-print "Comecou programa! \n\n"
-IP = 'localhost'
-porta_proxy = 2049
+IP = '127.0.0.1'
+porta_proxy = 2041
 porta_destino = 80
 maximo_dados = 1024
 conexao_pendente = 100
@@ -131,6 +105,7 @@ whitelist = whitelist_string.split('\n')
 arq.close()
 arq=open('badterms.txt', 'r')
 termo_string = arq.read()
+termo_string = termo_string.upper()
 termo = termo_string.split('\n')
 arq.close()
 urls = [] 
@@ -155,7 +130,6 @@ while True:
 		if not pedido: break
 		print pedido
 		thread.start_new_thread( proxy, (conexao, cliente, pedido, ) )
-		print "\n\n\n\n\n\n\n\n\nEXIT!!!\n\n\n\n\n\n\n\n\n"
 	except KeyboardInterrupt:
 		if servidor:
 			servidor.close()
